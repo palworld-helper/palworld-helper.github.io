7bb8f8eb8ca7cf9ee7c47c0523f50dd0a4cf47f5
taha <rtdonuk@gmail.com>
2024-02-05 20:28:55 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <div style={{textAlign: "center", maxWidth: "300px"}}>
+                <img src="images/lamball.png" alt="logo" />
+                <h4>Heyyo There !</h4>
+                <div>
+                    This is an application hosting test. If you reached here for some reason, our site is not published yet. But we are working on it to make it works as soon as possible.
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
